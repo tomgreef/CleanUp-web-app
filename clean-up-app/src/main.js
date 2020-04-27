c1ddf4bb53737c66c21df7613f69ad4d1dde92ff
Ruben <megarex333@gmail.com>
2020-04-27 20:22:40 +0200

    Buefy
    Añadido Buefy, Bulma con componentes de Vue

--- clean-up-app/src/main.js
+++ clean-up-app/src/main.js
@@ -3,6 +3,10 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import Buefy from "buefy";
+import "./assets/scss/app.scss";
+
+Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
