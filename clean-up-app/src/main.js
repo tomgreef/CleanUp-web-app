4dc18c81ef22dfe64f57cb9fed064bc136955a3c
Ruben <megarex333@gmail.com>
2020-04-28 16:53:42 +0200

    Firebase y configuracion
    Añadida la dependencia de firebase y configurado el linter

--- clean-up-app/src/main.js
+++ clean-up-app/src/main.js
@@ -1,17 +1,31 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import Buefy from "buefy";
-import "./assets/scss/app.scss";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Buefy from 'buefy';
+import firebase from 'firebase';
+import './assets/scss/app.scss';
 
 Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+	apiKey: 'AIzaSyCEZ-xMjsoJlJyyeMdL3rNiHA-454Nxres',
+	authDomain: 'clean-up-e3ad6.firebaseapp.com',
+	databaseURL: 'https://clean-up-e3ad6.firebaseio.com',
+	projectId: 'clean-up-e3ad6',
+	storageBucket: 'clean-up-e3ad6.appspot.com',
+	messagingSenderId: '223341924469',
+	appId: '1:223341924469:web:069b6c81d1c7cc2601283c',
+	measurementId: 'G-EPFMYW0M7H'
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
