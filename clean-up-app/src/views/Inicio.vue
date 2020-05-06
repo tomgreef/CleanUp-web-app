e40557912f85848cd0fef0afc858aa8d43ef8991
Ruben <megarex333@gmail.com>
2020-05-06 16:11:43 +0200

    Inicio
    Limpiadas paginas por defecto
    Creado el componente Hero
    Estilo actualizado
    Pagina de inicio creada

---
+++ clean-up-app/src/views/Inicio.vue
@@ -0,0 +1,19 @@
+<template>
+	<section class="section">
+		<div class="container">
+			<b-tabs
+				position="is-centered"
+				class="block"
+				type="is-toggle"
+				expanded
+			>
+				<b-tab-item label="Iniciar sesión">
+					<!-- TODO: Formulario -->
+				</b-tab-item>
+				<b-tab-item label="Crear cuenta">
+					<!-- TODO: Formulario -->
+				</b-tab-item>
+			</b-tabs>
+		</div>
+	</section>
+</template>
