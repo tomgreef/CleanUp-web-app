23f05c160e826bcc2bf2ac117f7e1973eee307c0
Sermorrec <sermorrec@alu.uma.es>
2020-05-08 18:36:16 +0200

    error

--- clean-up-app/src/components/UserLogin.vue
+++ clean-up-app/src/components/UserLogin.vue
@@ -2,17 +2,39 @@
 	<div class="box">
 		<h1 class="title is-4">Inicio de sesión</h1>
 		<b-field label="Email" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="email"></b-input>
 		</b-field>
 		<b-field label="Contraseña" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="pass"></b-input>
 		</b-field>
-		<b-button class="button" type="is-primary" expanded
+		<b-button @click="inicio" class="button" type="is-primary" expanded
 			>Iniciar sesión</b-button
 		>
 	</div>
 </template>
 
 <script>
-	export default {};
+	import firebase from 'firebase';
+	export default {
+		data: () => ({
+			email: '',
+			pass: ''
+		}),
+		methods: {
+			verify: () => {
+				if (this.email.includes('@')) {
+					console.log(this.email);
+				}
+			},
+			inicio: () => {
+				const auth = firebase.auth();
+				console.log(this.email);
+				auth.signinWithEmailAndPassword(this.email, this.pass).catch(
+					error => {
+						console.log(error);
+					}
+				);
+			}
+		}
+	};
 </script>
