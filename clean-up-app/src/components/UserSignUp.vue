1b768e25c9b4f184d9ac663d5d32b31680b459ee
Sermorrec <sermorrec@alu.uma.es>
2020-05-08 19:18:33 +0200

    sistema creacion cuenta

--- clean-up-app/src/components/UserSignUp.vue
+++ clean-up-app/src/components/UserSignUp.vue
@@ -2,23 +2,45 @@
 	<div class="box">
 		<h1 class="title is-4">Crear cuenta</h1>
 		<b-field label="Nombre" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="name"></b-input>
 		</b-field>
 		<b-field label="Telefono" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="tel"></b-input>
 		</b-field>
 		<b-field label="Email" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="email"></b-input>
 		</b-field>
 		<b-field label="Contraseña" label-position="on-border">
-			<b-input></b-input>
+			<b-input v-model="pass"></b-input>
 		</b-field>
-		<b-button class="button" type="is-primary" expanded
+		<b-button class="button" type="is-primary" expanded @click="signup"
 			>Crear cuenta</b-button
 		>
 	</div>
 </template>
 
 <script>
-	export default {};
+import firebase from "firebase"
+	export default {
+		data:() => ({
+			name: null,
+			tel: null,
+			email: null,
+			pass: null
+		}),
+		methods:{
+			signup: function(){
+				const auth = firebase.auth
+				auth().createUserWithEmailAndPassword(this.email, this.pass).then((u, err) =>{
+					if(err){
+						console.log(err);
+						
+					}else{
+						console.table(u)
+						u.updateProfile({telephone:this.tel})
+					}
+				})
+			}
+		}
+	};
 </script>
