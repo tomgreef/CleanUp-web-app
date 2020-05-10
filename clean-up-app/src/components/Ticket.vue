809899310f1232e06f24d763358b2ca987166b3b
Tom van Greevenbroek <tomgreef@icloud.com>
2020-05-10 16:20:48 +0200

    Componente - Ticket

---
+++ clean-up-app/src/components/Ticket.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="media">
+        <figure class="media-left">
+            <img src="https://bulma.io/images/placeholders/480x320.png">
+        </figure>
+        <div>
+            <div class="media-content">
+                <div class="content">
+                    <p class="title">Título</p>
+                    <p>Descripción</p>
+                    <p>Autor</p>
+                </div>
+                <nav class="level is-mobile">
+                    <div class="level-left">
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fas fa-reply"></i></span>
+                        </a>
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+                        </a>
+                        <a class="level-item">
+                            <span class="icon is-small"><i class="fas fa-heart"></i></span>
+                        </a>
+                    </div>
+                </nav>
+            </div>
+        </div> 
+    </div>
+</template>
---
+++ clean-up-app/src/views/Ticket.vue
@@ -0,0 +1,18 @@
+<template>
+	<section class="section">
+		<div class="container">
+			<Ticket>
+				<p>Hola</p>
+			</Ticket>
+		</div>
+	</section>
+</template>
+
+<script>
+    import Ticket from '@/components/Ticket';
+    export default {
+		components: {
+			Ticket
+		}
+	};
+</script>
