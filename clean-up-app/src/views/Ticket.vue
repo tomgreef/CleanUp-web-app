78bc72b82a48ac557356d26f3ab525503631e7ef
Tom van Greevenbroek <tomgreef@uma.es>
2020-05-11 19:57:05 +0200

    BarraNavegacion Inicio

--- clean-up-app/src/views/Ticket.vue
+++ clean-up-app/src/views/Ticket.vue
@@ -2,24 +2,16 @@
 	<section class="section">
 		<div class="container">
 			<Ticket>
-			</Ticket>
-			<BotonAgregarTicket>
-			</BotonAgregarTicket>
-			<BotonPerfil>
-			</BotonPerfil>
+			</Ticket>		
 		</div>
 	</section>
 </template>
 
 <script>
 	import Ticket from '@/components/Ticket';
-	import BotonAgregarTicket from '@/components/BotonAgregarTicket';
-	import BotonPerfil from '@/components/BotonPerfil';
     export default {
 		components: {
-			Ticket,
-			BotonAgregarTicket,
-			BotonPerfil
+			Ticket
 		}
 	};
 </script>
