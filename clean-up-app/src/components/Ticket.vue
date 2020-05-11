7a90ad23486da77f6dbcfc277dc6baece8689141
Tom van Greevenbroek <tomgreef@uma.es>
2020-05-11 19:23:40 +0200

    Ticket

--- clean-up-app/src/components/Ticket.vue
+++ clean-up-app/src/components/Ticket.vue
@@ -7,22 +7,8 @@
             <div class="media-content">
                 <div class="content">
                     <p class="title">Título</p>
-                    <p>Descripción</p>
-                    <p>Autor</p>
+                    <p class="description">Descripción</p>
                 </div>
-                <nav class="level is-mobile">
-                    <div class="level-left">
-                        <a class="level-item">
-                            <span class="icon is-small"><i class="fas fa-reply"></i></span>
-                        </a>
-                        <a class="level-item">
-                            <span class="icon is-small"><i class="fas fa-retweet"></i></span>
-                        </a>
-                        <a class="level-item">
-                            <span class="icon is-small"><i class="fas fa-heart"></i></span>
-                        </a>
-                    </div>
-                </nav>
             </div>
         </div> 
     </div>
