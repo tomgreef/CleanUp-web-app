49eda9ee560f850ce5116be426b1ffb3663bd815
José Luis Castro Díaz <jlcastro@uma.es>
2020-05-18 19:30:52 +0200

    Consultas a la base de datos
    fichero de funciones de consulta a la base de datos

---
+++ clean-up-app/src/helpers/dbHelper.js
@@ -0,0 +1,57 @@
+import firebase from 'firebase';
+
+export function simpleRead(table, field, operator, value) {
+    let v = []
+        firebase
+        .firestore()
+        .collection(table)
+        .where(field, operator, value)
+        .get()
+        .then(snap => {
+            snap.forEach(b => {
+                v.push(b.data());
+            });
+        });
+    return v
+}
+
+export function compositeRead(table, field1, operator1, value1, field2, operator2, value2) {
+    let v = []
+        firebase
+        .firestore()
+        .collection(table)
+        .where(field1, operator1, value1)
+        .where(field2, operator2, value2)
+        .get()
+        .then(snap => {
+            snap.forEach(b => {
+                v.push(b.data());
+            });
+        });
+    return v
+}
+
+export function readAllTable(table) {
+    let v = []
+        firebase
+        .firestore()
+        .collection(table)
+        .get()
+        .then(snap => {
+            snap.forEach(b => {
+                v.push(b.data());
+            });
+        });
+    return v
+}
+
+export function newUserEntry(name, email, phone, location, cp) {
+    firebase
+    .firestore()
+    .collection('users')
+    .set({
+        location: location,
+        cp: cp,
+        type: "user"
+    });
+}
