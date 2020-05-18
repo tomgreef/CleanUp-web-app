75822459bafeec30a8a6570c7521f085c40c76bc
José Luis Castro Díaz <jlcastro@uma.es>
2020-05-18 21:35:24 +0200

    Guardar ticket en la base de datos
    Falta la carga del archivo en el método uploadImages()

--- clean-up-app/src/components/CrearTicket.vue
+++ clean-up-app/src/components/CrearTicket.vue
@@ -2,7 +2,13 @@
 	<section class="columns box">
 		<div class="column is-one-third">
 			<b-field>
-				<b-upload v-model="dropFiles" :disabled="validImg" multiple drag-drop expanded>
+				<b-upload
+					v-model="dropFiles"
+					:disabled="validImg"
+					multiple
+					drag-drop
+					expanded
+				>
 					<section class="section">
 						<div class="content has-text-centered">
 							<p>
@@ -46,7 +52,7 @@
 				<b-input
 					maxlength="250"
 					type="textarea"
-                    placeholder="Ej: El contenedor de reciclaje en el Arroyo de la Miel está..."
+					placeholder="Ej: El contenedor de reciclaje en el Arroyo de la Miel está..."
 					v-model="descripcion"
 				></b-input>
 			</b-field>
@@ -66,18 +72,18 @@
 									placeholder="Nº"
 									v-model="numeroCalle"
 									:controls="false"
-									min=0
+									min="0"
 								></b-numberinput>
 							</div>
 
-							<div class="column is-2">	
+							<div class="column is-2">
 								<b-numberinput
 									placeholder="Código Postal"
 									v-model="CP"
 									:controls="false"
-									min=29000
+									min="29000"
 								></b-numberinput>
-							</div>	
+							</div>
 						</div>
 					</b-field>
 				</div>
@@ -89,6 +95,7 @@
 </template>
 
 <script>
+	import firebase from 'firebase';
 	export default {
 		data() {
 			return {
@@ -106,21 +113,48 @@
 				return (
 					this.titulo.length < 5 ||
 					this.descripcion.length < 20 ||
-                    this.dropFiles.length < 1 || 
-                    this.latitud == null ||
+					this.dropFiles.length < 1 ||
+					this.latitud == null ||
 					this.longitud == null ||
 					this.CP == null
 				);
-            },
-            validImg: function() {
-				return (
-					this.dropFiles.length > 2
-				);
+			},
+			validImg: function() {
+				return this.dropFiles.length > 2;
 			}
 		},
 		methods: {
 			deleteDropFile(index) {
 				this.dropFiles.splice(index, 1);
+			},
+
+			uploadImages() {
+				let imageReferences = [];
+				this.dropFiles.foreach(element => {
+					let ref = firebase.storage().ref();
+					let imageRef = ref.child('tickets/' + element);
+					imageReferences.push(imageRef);
+					ref.put(element);
+				});
+				this.createTicket(this.imageReferences);
+			},
+
+			createTicket(imageReferences) {
+				let uid = firebase.auth().currentUser.uid;
+				firebase
+					.firestore()
+					.collection('tickets')
+					.add({
+						title: this.titulo,
+						description: this.descripcion,
+						images: imageReferences,
+						street: this.calle,
+						streetNumber: this.numeroCalle,
+						CP: this.CP,
+						user_uid: this.uid,
+						agent_uid: null,
+						closed: false
+					});
 			}
 		}
 	};
