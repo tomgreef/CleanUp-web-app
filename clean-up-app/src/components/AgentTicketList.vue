618b7295cfe30cd85f33868d54f80953bcc61572
Ruben <megarex333@gmail.com>
2020-05-27 17:27:54 +0200

    Asignacion, cierre y filtrado de tickets
    TODO:
    Separar el listado en componentes para integrarlo para los usuarios, buscar errores que no se hayan detectado

--- clean-up-app/src/components/AgentTicketList.vue
+++ clean-up-app/src/components/AgentTicketList.vue
@@ -1,40 +1,50 @@
 <template>
 	<div>
-		<div class="buttons">
+		<b-field grouped group-multiline>
 			<p class="control">
-				<b-button type="is-primary">Asignar</b-button>
-				<b-button type="is-danger">Cerrar</b-button>
+				<b-button type="is-primary" @click="asign">Asignar</b-button>
+			</p>
+			<p class="control">
+				<b-button type="is-danger" @click="close">Cerrar</b-button>
+			</p>
+			<p class="control">
 				<b-switch
-					v-model="filter"
+					v-model="filterAgent"
 					:rounded="false"
 					size="is-medium"
 					type="is-primary"
 					>{{
-						filter ? 'Asignadas a mi' : 'Todas las incidencias'
+						filterAgent ? 'Asignadas a mi' : 'Todas las incidencias'
 					}}</b-switch
 				>
 			</p>
-		</div>
+			<p class="control">
+				<b-switch
+					v-model="filterClosed"
+					:rounded="false"
+					size="is-medium"
+					type="is-primary"
+					>{{
+						filterClosed ? 'Mostrar abiertas' : 'Mostrar cerradas'
+					}}</b-switch
+				>
+			</p>
+		</b-field>
 		<b-table
-			:data="tickets"
+			:data="filteredTickets"
 			paginated
 			per-page="10"
 			checkable
 			:checked-rows.sync="selection"
-			aria-next-label="Página siguiente"
-			aria-previous-label="Página anterior"
-			aria-page-label="Página"
-			aria-current-label="Página actual"
 			hoverable
 		>
 			<template slot-scope="props">
-				<b-table-column
-					field="title"
-					label="Título"
-					sortable
-					searchable
-				>
+				<b-table-column field="title" label="Título" sortable>
 					<PopUpTicketAgente :ticket="props.row" />
+				</b-table-column>
+
+				<b-table-column field="closed" label="Estado" centered>
+					{{ props.row.closed ? 'Cerrado' : 'Abierto' }}
 				</b-table-column>
 
 				<b-table-column
@@ -44,10 +54,13 @@
 				>
 					{{
 						props.row.agentUid != ''
-							? props.row.agentUid
+							? props.row.agentUid == currentUserUid
+								? 'Yo'
+								: props.row.agentUid
 							: 'Sin asignar'
 					}}
 				</b-table-column>
+
 				<b-table-column field="date" label="Fecha" sortable centered>
 					<span class="tag is-success">
 						{{ new Date(props.row.date).toLocaleDateString() }}
@@ -58,10 +71,12 @@
 				<section class="section">
 					<div class="content has-text-grey has-text-centered">
 						<p>
-							<b-icon icon="emoticon-sad" size="is-large">
-							</b-icon>
+							<b-icon
+								icon="package-variant"
+								size="is-large"
+							></b-icon>
 						</p>
-						<p>No hay incidencias</p>
+						<p>Nada por aquí</p>
 					</div>
 				</section>
 			</template>
@@ -70,16 +85,57 @@
 </template>
 
 <script>
-	import { db } from '@/firebase';
+	import { auth, db } from '@/firebase';
+	import { success } from '@/helpers/notificaciones';
 	import PopUpTicketAgente from '@/components/PopUpTicketAgente';
 
 	export default {
 		data: () => ({
 			selection: [],
-			filter: false
+			filterAgent: false,
+			filterClosed: false
 		}),
 		components: {
 			PopUpTicketAgente
+		},
+		computed: {
+			currentUserUid() {
+				return auth.currentUser.uid;
+			},
+			filteredTickets() {
+				return this.tickets.filter(
+					t =>
+						(this.filterAgent
+							? t.agentUid == auth.currentUser.uid
+							: true) && (this.filterClosed ? t.closed : true)
+				);
+			}
+		},
+		methods: {
+			update(action, condition) {
+				let ticketsRef = db.collection('tickets');
+				let updatePromises = [];
+				this.selection.forEach(t => {
+					if (condition(t)) {
+						updatePromises.push(
+							ticketsRef.doc(t.id).update(action)
+						);
+					}
+				});
+				Promise.all(updatePromises).then(
+					success('Acción realizada con éxito')
+				);
+				this.selection = [];
+			},
+			asign() {
+				this.update(
+					{ agentUid: auth.currentUser.uid },
+					t => t.agentUid == ''
+				);
+			},
+			close() {
+				this.update({ closed: true }, t => t.closed == false);
+			}
 		},
 		firestore() {
 			return {
