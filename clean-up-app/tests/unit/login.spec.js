4deb2f876d211e0b83cb08f68bd2e79a8f5d3d1d
Sermorrec <sermorrec@alu.uma.es>
2020-05-31 17:52:52 +0200

    Test unitario LoginForm

--- clean-up-app/tests/unit/login.spec.js
+++ clean-up-app/tests/unit/login.spec.js
@@ -1,12 +1,24 @@
 import { shallowMount } from "@vue/test-utils";
 import { expect } from "chai";
 import LoginForm from '@/components/LoginForm.vue';
+import { auth } from '@/firebase';
 
 describe("LoginForm.vue", function() {
     let component = shallowMount(LoginForm);
-    it('should throw error', function() {
+    it('should throw error when wrong email', function() {
         expect(function(){
-            component.inicio("hffhfhfhfh", "gdgdgdgdgdgd").to.throw('auth/invalid-email');
+            component.inicio("hffhfhfhfh", "randompassword").to.throw('auth/invalid-email');
         })
+    });
+    it('should throw error when user does not exists', function() {
+        expect(function(){
+            component.inicio("random@gmail.com", "randompassword").to.throw('auth/user-not-found');
+        })
+    });
+    it('should throw error when wrong password', function() {
+        auth.createUserWithEmailAndPassword("random@gmail.com", "randompassword");
+        expect(function(){
+             component.inicio("random@gmail.com", "wrongpassword").to.throw('auth/wrong-password');
+        });
     });
 });
