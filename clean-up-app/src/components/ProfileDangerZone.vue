58306868db2753476bc86234389a31473b969a4e
Tom van Greevenbroek <tomgreef@uma.es>
2020-05-31 17:59:47 +0200

    Delete

--- clean-up-app/src/components/ProfileDangerZone.vue
+++ clean-up-app/src/components/ProfileDangerZone.vue
@@ -1,5 +1,25 @@
 <template>
 	<div class="buttons level-right">
-		 <button class="button is-danger">Eliminar Cuenta</button>
+		 <button class="button is-danger" @click="confirmCustomDelete">
+                Eliminar Perfil
+            </button>
 	</div>
 </template>
+
+<script>
+    export default {
+        methods: {
+            confirmCustomDelete() {
+                this.$buefy.dialog.confirm({
+                    title: 'Eliminando cuenta',
+                    message: '¿Estas seguro que quieres <b>eliminar</b> tu cuenta? Esta acción no se puede deshacer.',
+                    confirmText: 'Eliminar Cuenta',
+                    type: 'is-danger',
+                    hasIcon: true,
+					onConfirm: () => this.$buefy.toast.open('¡Cuenta Eliminada!'),
+					cancelText: 'Cancelar'
+                })
+            }
+        }
+    }
+</script>
